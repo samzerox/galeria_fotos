e88db759f475bf0f52aa0a536842c90f6d929df7
samzerox <sam.zerox88@gmail.com>
2018-03-13 12:59:44 -0600

    Agregamos bootstrap a la aplicacion

--- public/layouts/header.php
+++ public/layouts/header.php
@@ -4,6 +4,7 @@
 <meta charset="utf-8">
 <title>Galeria de Fotos</title>
 <link href="styles/main.css" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>

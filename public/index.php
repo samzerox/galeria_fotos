e9172b0df8d7b8e2262f8424637db4de6ec98125
samzerox <sam.zerox88@gmail.com>
2018-03-13 13:24:19 -0600

    Errores corregidos, ahora funciona en php7

--- public/index.php
+++ public/index.php
@@ -14,42 +14,42 @@
 	$fotos = Foto::buscar_por_sql($sql);
 ?>
 <?php incluir_plantillas("header.php"); ?>
-<div id="cuerpo">
-<h2>Todas las Fotos</h2>
-<?php foreach($fotos as $foto){ ?>
-<div style="float:left; margin:10px;">
-<a href="full_screen.php?id=<?php echo $foto->id; ?>"><img src="<?php echo $foto->ruta_imagen(); ?>" width="100" /></a>
-    <p><?php echo $foto->titulo; ?></p>
-</div>
-<?php }?>
-<div style="clear:both;">
-<?php
-	if($paginacion->existe_anterior())
-	{
-		echo "<a href=\"index.php?pagina=";
-		echo $paginacion->pagina_anterior();
-		echo "\">Anterior</a>";
-	}
-	
-	for($i=1;$i<=$paginacion->total_paginas();$i++)
-	{
-		if($paginacion->pagina_actual == $i)
-		{
-			echo "&nbsp;<b>{$i}</b>";
-		}
-		else
-		{
-			echo "&nbsp;<a href=\"index.php?pagina={$i}\"> {$i} -</a>";
-		}
-	}
-	
-	if($paginacion->existe_siguiente())
-	{
-		echo "<a href=\"index.php?pagina=";
-		echo $paginacion->pagina_siguiente();
-		echo "\">Siguiente</a>";
-	}
-?>
-</div>
+<div id="cuerpo" align="center">
+	<h2>Todas las Fotos</h2>
+	<div class="col-md-12" style="float:center; margin:10px;">
+	<?php foreach($fotos as $foto){ ?>
+		<a href="full_screen.php?id=<?php echo $foto->id; ?>"><img src="<?php echo $foto->ruta_imagen(); ?>" width="100" /></a>
+		<p><?php echo $foto->titulo; ?></p>
+		<?php }?>
+	</div>
+	<div style="clear:both;">
+		<?php
+			if($paginacion->existe_anterior())
+			{
+				echo "<a href=\"index.php?pagina=";
+				echo $paginacion->pagina_anterior();
+				echo "\">Anterior</a>";
+			}
+			
+			for($i=1;$i<=$paginacion->total_paginas();$i++)
+			{
+				if($paginacion->pagina_actual == $i)
+				{
+					echo "&nbsp;<b>{$i}</b>";
+				}
+				else
+				{
+					echo "&nbsp;<a href=\"index.php?pagina={$i}\"> {$i} -</a>";
+				}
+			}
+			
+			if($paginacion->existe_siguiente())
+			{
+				echo "<a href=\"index.php?pagina=";
+				echo $paginacion->pagina_siguiente();
+				echo "\">Siguiente</a>";
+			}
+		?>
+	</div>
 </div>
 <?php incluir_plantillas("footer.php"); ?>
